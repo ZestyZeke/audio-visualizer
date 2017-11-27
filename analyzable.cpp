9622988a0579832b70018b951946036e9b210d65
zestze <eer2138@columbia.edu>
2017-11-26 21:14:16 -0500

    realized copying wrong version, 2.4 not 2.3, so going back to change stuff

--- analyzable.cpp
+++ analyzable.cpp
@@ -67,6 +67,55 @@ namespace sf
 
         void Analyzable::setLoopPoints(TimeSpan timePoints)
         {
-                // left off here.
+                Span<Uint64> samplePoints(timeToSamples(timePoints.offset),
+                                timeToSamples(timePoints.length));
+                if (getChannelCount() == 0 || m_file.getSampleCount() == 0) {
+                        sf::err() << "Analyzable is not in a valid state to";
+                        sf::err() << " assign Loop Points." << std::endl;
+                        return;
+                }
+
+                samplePoints.offset += (getChannelCount() - 1);
+                samplePoints.offset += (samplePoints.offset % getChannelCount());
+                samplePoints.length += (getChannelCount() - 1);
+                samplePoints.length += (samplePoints.length % getChannelCount());
+
+                if (samplePoints.offset >= m_file.getSampleCount()) {
+                        sf::err() << "LoopPoints offset val must be in range";
+                        sf::err() << " [0, Duration)." << std::endl;
+                        return;
+                }
+                if (samplePoints.length == 0) {
+                        sf::err() << "LoopPoints length val must be nonzero.";
+                        sf::err() << std::endl;
+                        return;
+                }
+
+                samplePoints.length = std::min(samplePoints.length,
+                                m_file.getSampleCount() - samplePoints.offset);
+
+                if (samplePoints.offset == m_loopSpan.offset && samplePoints.length == m_loopSpan.length)
+                        return;
+
+                Status oldStatus = getStatus();
+                Time oldPos = getPlayingOffset();
+
+                stop();
+
+                m_loopSpan = samplePoints;
+
+                if (oldPos != Time::Zero)
+                        setPlayingOffset(oldPos);
+
+                if (oldStatus == Playing)
+                        play();
+        }
+
+        bool Analyzable::onGetData(SoundStream::Chunk& data)
+        {
+                Lock lock(m_mutex);
+
+                std::size_t toFill = m_samples.size();
+                Uint64 currentOffset = m_file.getSampleOffset();
         }
 }
